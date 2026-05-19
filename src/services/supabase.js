867c1a53c3e95909007edd6e0662db05b8fe422f
Lyhour Houn <Hour0598@gmail.com>
2026-05-19 22:01:47 +0700

    Completed using supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://womigkogshdrrvnqzfvw.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbWlna29nc2hkcnJ2bnF6ZnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTI4NjAsImV4cCI6MjA5NDY4ODg2MH0.QXFcmwUwtvsh57p8MVvegaFEod9ZAUBhAnKqSE5HWdQ";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
